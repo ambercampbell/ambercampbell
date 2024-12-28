77a7a9031c040e7c47b9ad8bd7723809e818befb
ambercampbell <98134947+ambercampbell@users.noreply.github.com>
2024-12-28 01:41:32 -0600

    fixing buttons

--- js/custom.js
+++ js/custom.js
@@ -42,36 +42,3 @@
 
 })(jQuery);
 
-/*=========================================================================
-		Navigation Functions
-	=========================================================================*/
-	$('.section-toggle').on('click', function(){
-		var $this = $(this),
-			sect = $( '#' + $this.data('section') ),
-			current_sect = $('.section.active');
-		if(sect.length == 1){
-			if( sect.hasClass('active') == false && $('body').hasClass('section-switching') == false ){
-				$('body').addClass('section-switching');
-				if( sect.index() < current_sect.index() ){
-					$('body').addClass('up');
-				}else{
-					$('body').addClass('down');
-				}
-				setTimeout(function(){
-					$('body').removeClass('section-switching up down');			
-				}, 2500);
-				setTimeout(function(){
-					current_sect.removeClass('active');
-					sect.addClass('active');
-				}, 1250);
-				if( sect.hasClass('border-d') ){
-					$('body').addClass('border-dark');
-				}else{
-					$('body').removeClass('border-dark');
-				}
-			}
-		}
-	});
-	
-
-
